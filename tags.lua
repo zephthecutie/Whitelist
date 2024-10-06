5ca2b850723d2e4d164bff63c28f5a0fd5166ad0
zephthecutie <trollageforfunnies@gmail.com>
2024-10-06 12:58:49 -0700

    Create tags.lua

---
+++ tags.lua
@@ -0,0 +1,6 @@
+return {
+    [1] = {name = "CARBON OWNER", color = Color3.fromRGB(178, 0, 0)},
+    [2] = {name = "TEST", color = Color3.fromRGB(255, 76.5, 76.5)},
+    [3] = {name = "MOD", color = Color3.fromRGB(3, 69, 252)},
+}
+
